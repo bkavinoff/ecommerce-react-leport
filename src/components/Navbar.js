3b6cd66444f691e13b85461c6c4fefee4a2940a4
bkavinoff <bkavinoff@gmail.com>
2022-03-15 02:16:17 -0300

    Adding main menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+// import { green } from '@mui/material/colors';
+// const color = green[500];
+
+function Navbar(){
+    return(
+        <header className='main-header'>
+            <div className='container-logo'>
+                <img src='/img/leport-logo.png' className='img-header' alt='logo' />
+            </div>
+            <ul className='navbar'>
+                <li><Button variant="text" color="success">Home</Button></li>
+                <li><Button variant="text" color="success">Productos</Button></li>
+                <li><Button variant="text" color="success">Nosotros</Button></li>
+                <li><Button variant="text" color="success">Contacto</Button></li>
+            </ul>
+        </header>
+
+    )
+}
+
+export default Navbar
